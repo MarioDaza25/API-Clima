b368bc4b86d0db828caa2d17056026be6059a5b7
julianlpz69 <julianjoselpz2019@gmail.com>
2023-07-06 16:59:45 -0500

    Nuevos Cambios

--- clima.js
+++ clima.js
@@ -5,7 +5,6 @@ async function fetchData(latitud, longitud) {
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=25153e2e94aa1b62fb1e91e29b544298`);
       const data = await response.json();
-      console.log(data);
       return data;
     } catch (error) {
       throw 'Error al obtener los datos';
@@ -24,11 +23,12 @@ async function fetchData(latitud, longitud) {
       dataContainer.innerHTML = ''
       let contenidoTabla = `
         <div class="card">
-          <img src="./imagenes/clima.avif" class="card-img-top" alt="clima">
+          <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" class="card-img-top" alt="clima">
           <div class="card-body">
             <h3>${data.name}</h3>
             <p class="card-text">
               Temperatura: ${Math.round((data.main.temp)-273.15)} °C<br>
+              Viento: ${data.wind.speed} k/h<br>
               Latitud: ${data.coord.lat} <br>
               Longitud: ${data.coord.lon} <br>
               Pais: ${data.sys.country}
